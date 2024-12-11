aaf1ca74ff95d218dd67b3cdea08a971e2aa5c73
Puncharasx <puncharasx@gmail.com>
2024-12-11 15:16:20 +0700

    implement part2 of day2

--- 2024/day2/javascript/index.js
+++ 2024/day2/javascript/index.js
@@ -42,6 +42,50 @@ function main() {
   }
 
   console.log('Part 1 : ', resultPart1)
+
+  let resultPart2 = 0
+
+  for (let i = 0; i < array2d.length; i++) {
+    const levels = array2d[i]
+    let isSafe = true
+
+    const isSafeReport = (levels) => {
+      let isIncreasing = null
+      for (let j = 0; j < levels.length - 1; j++) {
+        const difference = levels[j + 1] - levels[j]
+
+        if (Math.abs(difference) < 1 || Math.abs(difference) > 3) {
+          return false
+        }
+
+        const currentTrend = difference > 0
+        if (isIncreasing === null) {
+          isIncreasing = currentTrend
+        } else if (isIncreasing !== currentTrend) {
+          return false
+        }
+      }
+      return true
+    }
+
+    if (!isSafeReport(levels)) {
+      isSafe = false
+
+      for (let j = 0; j < levels.length; j++) {
+        const modifiedLevels = levels.slice(0, j).concat(levels.slice(j + 1))
+        if (isSafeReport(modifiedLevels)) {
+          isSafe = true
+          break
+        }
+      }
+    }
+
+    if (isSafe) {
+      resultPart2 += 1
+    }
+  }
+
+  console.log('Part 2 : ', resultPart2)
 }
 
 main()
